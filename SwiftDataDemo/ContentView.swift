84fb9e4c7167d53626bfca4d727eb13a4773dd7c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 10:26:54 +0500

    Add the item to the dat bas eand getting it and fwith the query of list work done

--- SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var context
     
+    @Query private var items: [DataItem]
+    
     var body: some View {
         VStack {
             
@@ -18,12 +20,22 @@ struct ContentView: View {
             Button("Add an item") {
                 addItem()
             }
+            
+            List {
+                ForEach (items) { item in
+                    Text(item.name)
+                }
+            }
+            
         }
         .padding()
     }
     
     func addItem() {
-        
+        // Create the item
+        let item = DataItem(name: "Test Item")
+        // Add the item to the data context
+        context.insert(item)
     }
     
 }
