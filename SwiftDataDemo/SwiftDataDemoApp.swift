c5a99da0011667cac33303d130d70a11aad3aed9
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 10:10:19 +0500

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Elexoft on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
